b5544c12b50d02ea10fb1132c3e42013b8c253bd
Yasar Karaca <109455877+ysrkrc@users.noreply.github.com>
2023-01-16 20:05:01 +0300

    Add files via upload

---
+++ src/main/java/CodeSignal4.java
@@ -0,0 +1,21 @@
+package lambda;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class CodeSignal4 {
+    public static void main(String[] args) {
+//Bir tamsayı dizisi verildiğinde, en büyük çarpımı olan bitişik eleman çiftini bulun ve bu ürünü döndürün.
+
+        int inputArray[] = {-23, 4, -3, 8, -12};
+        int sum=0;
+        for (int i=0; i<inputArray.length-1;i++){
+
+          sum= Math.max(sum,inputArray[i]*inputArray[i+1]);
+
+
+        } System.out.println(sum);
+    }
+}
+
+

56def74f2c7457f21a5c0dcea0e56c6d3348e8b0
Yasar Karaca <109455877+ysrkrc@users.noreply.github.com>
2023-02-02 20:35:16 +0300

    Add files via upload

---
+++ CodeSignal8.java
@@ -0,0 +1,24 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CodeSignal8 {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        List<Integer> list= new ArrayList<>();
+        for (int w: arr){
+            list.add(w);
+        }
+        System.out.println(list);
+
+        Integer [] arr2= list.toArray(new Integer[list.size()]);
+        System.out.println(Arrays.toString(arr2));
+    }
+
+
+
+
+
+}
